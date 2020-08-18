0cff040176738da00332ac69150ae2eb447fe8e1
nbs <nathanofsmith@gmail.com>
2020-08-17 21:42:48 -0700

    map functionality

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, session
 from flask_googlemaps import GoogleMaps
+import folium
 
 app = Flask(__name__)
 
@@ -12,7 +13,9 @@ API_KEY = "AIzaSyDH1FdEzMrPOj3V1bxIwA5hdQmhP4brCaU"
 
 @app.route('/map')
 def map():
-    pass
+    start_coords = (46.9540700, 142.7360300)
+    folium_map = folium.Map(location=start_coords, zoom_start=14)
+    return folium_map._repr_html_()
 
 if __name__ == '__main__':
-    app.run(port=33507)
+    app.run(port=33507, debug=True)
