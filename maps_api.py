648418f954cf61db2b35a0f037695370c705a0b6
hn2OAtIP83 <31112128+hn2OAtIP83@users.noreply.github.com>
2020-08-25 02:58:54 -0400

    google maps api

---
+++ maps_api.py
@@ -0,0 +1,11 @@
+import googlemaps
+from datetime import datetime as dt
+
+file = open('api_key.txt', 'r')
+key = file.read()
+
+gmaps = googlemaps.Client(key)
+
+geo = gmaps.geocode('1600 Amphitheatre Parkway, Mountain View, CA')
+
+print(geo)
