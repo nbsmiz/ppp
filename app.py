600d022bd2f419501333d68bfe1c45082f8b0c55
nbsmiz <31112128+nbsmiz@users.noreply.github.com>
2020-08-26 21:31:12 -0400

    removed googlemaps lib

--- app.py
+++ app.py
@@ -1,17 +1,15 @@
 from flask import Flask, render_template, request, redirect, session
-from flask_googlemaps import GoogleMaps
 import folium
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def index():
     return render_template('index.html')
 
-API_KEY = "AIzaSyDH1FdEzMrPOj3V1bxIwA5hdQmhP4brCaU"
 
-@app.route('/map')
+@app.route('/map', methods=['POST'])
 def map():
     start_coords = (46.9540700, 142.7360300)
     folium_map = folium.Map(location=start_coords, zoom_start=14)
