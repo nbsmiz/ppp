694e90c9993b6f96ced7cc77e5e2fe51dc216862
nbsmiz <31112128+nbsmiz@users.noreply.github.com>
2020-08-28 00:47:29 -0400

    added search

--- app-1.py
+++ app-1.py
@@ -14,24 +14,16 @@ def index():
 
 @app.route('/map', methods=['POST', 'GET'])
 def map():
-    if (request.method == 'POST') and request.form.get('zipCode') != '':
-        zipCode = request.form.get('zipCode')
-    else:
-        zipCode = '06071'
-    g = geo.osm(zipCode + ' USA')  # geocode user input
+    g = geo.osm('06039 USA')  # geocode user input
     start_coords = g.latlng  # set view map center to latitude and longitude 
     m = f.Map(location = [0,0], zoom_start=12)  # location required in order to modify with user input
     m.location = start_coords  # change map view location to user input
     return m._repr_html_()
 
 
-@app.route('/map2', methods=['POST', 'GET'])
+@app.route('/map2')
 def map2():
-    if (request.method == 'POST') and request.form.get('zipCode') != '':
-        zipCode = request.form.get('zipCode')
-        return render_template('view_map.html', zipCode=zipCode)
-    else:
-        return render_template('view_map.html', zipCode='06071')
+    return render_template('view_map.html')
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
--- app.py
+++ app.py
@@ -14,16 +14,24 @@ def index():
 
 @app.route('/map', methods=['POST', 'GET'])
 def map():
-    g = geo.osm('06039 USA')  # geocode user input
+    if (request.method == 'POST') and request.form.get('zipCode') != '':
+        zipCode = request.form.get('zipCode')
+    else:
+        zipCode = '06071'
+    g = geo.osm(zipCode + ' USA')  # geocode user input
     start_coords = g.latlng  # set view map center to latitude and longitude 
     m = f.Map(location = [0,0], zoom_start=12)  # location required in order to modify with user input
     m.location = start_coords  # change map view location to user input
     return m._repr_html_()
 
 
-@app.route('/map2')
+@app.route('/map2', methods=['POST', 'GET'])
 def map2():
-    return render_template('view_map.html')
+    if (request.method == 'POST') and request.form.get('zipCode') != '':
+        zipCode = request.form.get('zipCode')
+        return render_template('view_map.html', zipCode=zipCode)
+    else:
+        return render_template('view_map.html', zipCode='06071')
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
